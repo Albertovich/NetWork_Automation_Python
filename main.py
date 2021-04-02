364ed30da60d729a659dbee64958a2070893ca0a
Alberto <albertofsanchez@gmail.com>
2021-04-02 22:43:19 +0200

    Bucle de switches

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ HOST = "192.168.122.72"
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
-tn = telnetlib.Telnet(HOST)
+f = open (myswitches)
 
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+for IP in f:
+        IP=IP.strip()
+        print ("Configuring Switch" + (IP))
+        HOST = IP
+        tn = telnetlib.Telnet(HOST)
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
 
-tn.write(b"enable\n")
-tn.write(b"cisco\n")
-tn.write(b"conf term\n")
+        if password:
+            tn.read_until(b"Password: ")
+            tn.write(password.encode('ascii') + b"\n")
+        tn.write(b"conf term\n")
 
-for n in range (2,11):
-    tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
-    tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
+        for n in range (2,21):
+            tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+            tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
+        tn.write(b"end\n")
+        tn.write(b"exit\n")
+        print(tn.read_all().decode('ascii'))
 
-tn.write(b"end\n")
-tn.write(b"wr\n")
-tn.write(b"exit\n")
 
-print(tn.read_all().decode('ascii'))
